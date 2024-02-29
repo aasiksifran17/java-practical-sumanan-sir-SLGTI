0d1d422166cfd43ca8f57a04cff6bf9f254c72c2
aasiksifran17 <aasikmohamed782@gmail.com>
2024-02-29 14:37:25 +0530

    first commit

---
+++ primenumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class primenumber
+ {
+    public static void main(String[] args) 
+    {
+        Scanner prime = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = prime.nextInt();
+
+        if (number % 10 == 0)
+         {
+            System.out.println(number + " is a multiple ");
+        }
+         else
+         {
+            System.out.println(number + " is not a multiple");
+        }
+    }
+}
---
+++ whileloop.java
@@ -0,0 +1,17 @@
+public class whileloop
+ {
+    public static void main(String[] args)
+     {
+        int x = 1;
+        int y = 1;
+
+        System.out.println();
+        while (x <= 10)
+         {
+            int a = x * x;
+            System.out.println(a);
+            x++;
+            
+        }
+    }
+}
